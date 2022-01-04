94aae05bd686ba195c367c8211db6c5a61b46a7d
bru1996 <49351158+bru1996@users.noreply.github.com>
2022-01-04 09:30:27 -0300

    Create quotes.js

---
+++ routes/quotes.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+const quotes = require('../services/quotes');
+
+/* GET quotes listing. */
+router.get('/', async function(req, res, next) {
+  try {
+    res.json(await quotes.getMultiple(req.query.page));
+  } catch (err) {
+    console.error(`Error while getting quotes `, err.message);
+    next(err);
+  }
+});
+
+module.exports = router;
+
+/* POST quotes */
+router.post('/', async function(req, res, next) {
+  try {
+    res.json(await quotes.create(req.body));
+  } catch (err) {
+    console.error(`Error while posting quotes `, err.message);
+    next(err);
+  }
+});
+
+module.exports = router;
+
+function validateCreate(quote) {
+  let messages = [];
+
+  console.log(quote);
+
+  if (!quote) {
+    messages.push('No object is provided');
+  }
+
+  if (!quote.quote) {
+    messages.push('Quote is empty');
+  }
+
+  if (!quote.author) {
+    messages.push('Quote is empty');
+  }
+
+  if (quote.quote && quote.quote.length > 255) {
+    messages.push('Quote cannot be longer than 255 characters');
+  }
+
+  if (quote.author && quote.author.length > 255) {
+    messages.push('Author name cannot be longer than 255 characters');
+  }
+
+  if (messages.length) {
+    let error = new Error(messages.join());
+    error.statusCode = 400;
+
+    throw error;
+  }
+}
+
+async function create(quote){
+  validateCreate(quote);
+
+  const result = await db.query(
+    'INSERT INTO quote(quote, author) VALUES ($1, $2) RETURNING *',
+    [quote.quote, quote.author]
+  );
+  let message = 'Error in creating quote';
+
+  if (result.length) {
+    message = 'Quote created successfully';
+  }
+
+  return {message};
+}
