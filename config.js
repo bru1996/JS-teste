a2336866db69e697fd1734c2947f82ddbbc04aa9
bru1996 <49351158+bru1996@users.noreply.github.com>
2022-01-04 20:20:28 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const env = process.env;
 
 const config = {
@@ -11,4 +12,19 @@ const config = {
   listPerPage: env.LIST_PER_PAGE || 10,
 };
 
+=======
+const env = process.env;
+
+const config = {
+  db: { /* do not put password or any sensitive info here, done only for demo */
+    host: env.DB_HOST || 'kesavan.db.elephantsql.com',
+    port: env.DB_PORT || '5432',
+    user: env.DB_USER || 'tbiltenr',
+    password: env.DB_PASSWORD || 'JStxQl_MKKDaaDJXA7ZHvRbV4OX83Lej',
+    database: env.DB_NAME || 'tbiltenr',
+  },
+  listPerPage: env.LIST_PER_PAGE || 10,
+};
+
+>>>>>>> 11a13e31f64404cb37f696f075f510b7a5489777
 module.exports = config;
