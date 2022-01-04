1ce4dd9859ab6c3c272fbfac48dea4daef2f6864
bru1996 <49351158+bru1996@users.noreply.github.com>
2022-01-04 20:20:25 -0300

    Update helper.js

--- helper.js
+++ helper.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function getOffset(currentPage = 1, listPerPage) {
   return (currentPage - 1) * [listPerPage];
 }
@@ -12,4 +13,20 @@ function emptyOrRows(rows) {
 module.exports = {
   getOffset,
   emptyOrRows
+=======
+function getOffset(currentPage = 1, listPerPage) {
+  return (currentPage - 1) * [listPerPage];
+}
+
+function emptyOrRows(rows) {
+  if (!rows) {
+    return [];
+  }
+  return rows;
+}
+
+module.exports = {
+  getOffset,
+  emptyOrRows
+>>>>>>> 11a13e31f64404cb37f696f075f510b7a5489777
 }
