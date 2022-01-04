4efa0c6f8e7add052dbaab282ec53d32da5c5cf9
bru1996 <49351158+bru1996@users.noreply.github.com>
2022-01-04 20:20:20 -0300

    Update quotes.js

--- services/quotes.js
+++ services/quotes.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const db = require('./db');
 const helper = require('../helper');
 const config = require('../config');
@@ -40,3 +41,47 @@ module.exports = {
   create
 }
 
+=======
+const db = require('./db');
+const helper = require('../helper');
+const config = require('../config');
+
+async function getMultiple(page = 1) {
+  const offset = helper.getOffset(page, config.listPerPage);
+  const rows = await db.query(
+    'SELECT id, quote, author FROM quote OFFSET $1 LIMIT $2', 
+    [offset, config.listPerPage]
+  );
+  const data = helper.emptyOrRows(rows);
+  const meta = {page};
+
+  return {
+    data,
+    meta
+  }
+}
+
+module.exports = {
+  getMultiple
+}
+
+async function create(quote) {
+  const result = await db.query(
+    'INSERT INTO quote(quote, author) VALUES ($1, $2) RETURNING *',
+    [quote.quote, quote.author]
+  );
+  let message = 'Error in creating quote';
+
+  if (result.length) {
+    message = 'Quote created successfully';
+  }
+
+  return {message};
+}
+
+module.exports = {
+  getMultiple,
+  create
+}
+
+>>>>>>> 11a13e31f64404cb37f696f075f510b7a5489777
